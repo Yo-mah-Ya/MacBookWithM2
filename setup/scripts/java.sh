0d8d788eb66199c1d66edb1c6e50493b4a9d6e1d
Yo-mah-Ya <shanzhongziye@gmail.com>
2025-04-17 22:50:08 -0400

    updated java.sh

--- setup/scripts/java.sh
+++ setup/scripts/java.sh
@@ -8,8 +8,12 @@ echo 'eval "$(jenv init -)"' >> ~/.bashrc
 source ~/.bashrc
 
 brew install java11
-
 jenv add /opt/homebrew/opt/openjdk@11/libexec/openjdk.jdk/Contents/Home
-jenv global 11.0
+brew install java17
+jenv add /opt/homebrew/opt/openjdk@17/libexec/openjdk.jdk/Contents/Home
+brew install java21
+jenv add /opt/homebrew/opt/openjdk@21/libexec/openjdk.jdk/Contents/Home
+
+jenv global 21.0
 jenv enable-plugin export
 source ~/.bashrc
